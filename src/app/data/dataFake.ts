0a277b66b72f50a35b34a8599b7475885c3a8cda
Che36 <tron36.cc@gmail.com>
2023-02-09 23:05:28 -0300

    feat(datafake): add a new propertie to datafake
    add a pharagraf to beter description of the image

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -3,25 +3,29 @@ export const dataFake = [
     "id": "1",
     "title": "NOVO HOMEM DE FERRO EM 3D",
     "description": "marvel anuncia um novo filme do homem de ferro, confira",
-    "photoCover": "https://trecobox.com.br/wp-content/uploads/2022/02/Robert-Downey-Jr.-Homem-de-Ferro-capa.jpg"
+    "photoCover": "https://trecobox.com.br/wp-content/uploads/2022/02/Robert-Downey-Jr.-Homem-de-Ferro-capa.jpg",
+    "pharagraf": "O artista conhecido como Rahan Nejraoui imaginou Tom Cruise como uma Variante do Homem de Ferro de Tony Stark. Agora que o Multiverso reina no MCU e temos várias variantes de personagens, como aconteceu na série Loki ou em Homem-Aranha: Sem Volta Para Casa, explorar a ideia de colocar Tom Cruise como uma variante do Homem de Ferro seria uma boa"
   },
   {
     "id": "2",
     "title": "Loki na Disney",
     "description": "Disney Anuncia o lançamento de Loki 2, previsto para Março 2023",
-    "photoCover": "https://images.thedirect.com/media/article_full/loki-season-2-filming.jpg"
+    "photoCover": "https://images.thedirect.com/media/article_full/loki-season-2-filming.jpg",
+    "pharagraf": "A série Loki, da Marvel, acaba de anunciar a janela de lançamento de sua 2ª temporada durante a San Diego Comic-Con 2022. Segundo o presidente do estúdio, Kevin Feige, a série do Disney+ deve retornar no verão de 2023 nos Estados Unidos, o que significa segundo semestre aqui no Brasil"
   },
   {
     "id": "3",
     "title": "Pantera Negra 3",
     "description": "",
-    "photoCover": "https://musicaecinema.com/wp-content/uploads/2023/02/letiti-fala-sobre-pantera-negra-2-chardwick-boseman-capa.jpeg.webp"
+    "photoCover": "https://musicaecinema.com/wp-content/uploads/2023/02/letiti-fala-sobre-pantera-negra-2-chardwick-boseman-capa.jpeg.webp",
+    "pharagraf": "Acho que já está em andamento. Você sabe, nós tivemos dois anos incríveis apenas lançando e todos se unindo para apoiá-lo. Precisamos de um pouco de pausa, precisamos nos reagrupar, e Ryan Coogler precisa voltar ao laboratório então vai demorar um pouco, mas estou muito animado para vocês verem isso"
   },
   {
     "id": "4",
     "title": "Novidades dos XMen",
     "description": "",
-    "photoCover": "https://legadodamarvel.com.br/wp-content/uploads/2021/01/Novo-filme-dos-X-Men-na-Marvel-Studios-ganha-novidades-legadodamarvel-750x422.jpg"
+    "photoCover": "https://legadodamarvel.com.br/wp-content/uploads/2021/01/Novo-filme-dos-X-Men-na-Marvel-Studios-ganha-novidades-legadodamarvel-750x422.jpg",
+    "pharagraf": "Feige basicamente tranquilizou os fãs dizendo que os mutantes estão sim em produção ativa dentro da Marvel, mas que ainda não é hora para anúncios oficiais. Pessoalmente, nós esperamos que possa haver novidades ainda este ano"
   }
 ]
 
--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import {dataFake} from '../../data/dataFake'
+import { dataFake } from '../../data/dataFake'
 
 @Component({
   selector: 'app-content',
@@ -8,29 +8,31 @@ import {dataFake} from '../../data/dataFake'
   styleUrls: ['./content.component.css']
 })
 export class ContentComponent implements OnInit {
-  photoCover:string = ""
-  contentTitle:string = ""
-  contentDescription:string = ""
-  private id:string | null = "0"
+  photoCover: string = ""
+  contentTitle: string = ""
+  contentDescription: string = ""
+  contentPharagraf: string = ""
+  private id: string | null = "0"
 
   constructor(
-    private route:ActivatedRoute
+    private route: ActivatedRoute
   ) { }
 
   ngOnInit(): void {
-    this.route.paramMap.subscribe( value =>
-     this.id = value.get("id")
+    this.route.paramMap.subscribe(value =>
+      this.id = value.get("id")
     )
 
     this.setValuesToComponent(this.id)
   }
 
-  setValuesToComponent(id:string | null){
+  setValuesToComponent(id: string | null) {
     const result = dataFake.filter(article => article.id == id)[0]
 
     this.contentTitle = result.title
     this.contentDescription = result.description
     this.photoCover = result.photoCover
+    this.contentPharagraf = result.pharagraf
   }
 
 }
