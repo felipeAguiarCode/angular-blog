09fd4e188ea7ff13679f2523cc114dee6bc7d2e1
Che36 <tron36.cc@gmail.com>
2023-02-09 22:24:22 -0300

    feat(component): add two more datas to dataFake file to populate the application

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,14 +1,29 @@
 export const dataFake = [
   {
-    "id":"1",
+    "id": "1",
     "title": "NOVO HOMEM DE FERRO EM 3D",
     "description": "marvel anuncia um novo filme do homem de ferro, confira",
-    "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/7F51FA9F6CBD9F0C9B1394B1CC0A6A842D07091318674E234CD33CBF7C28CDC3/scale?width=1200&aspectRatio=1.78&format=jpeg"
+    "photoCover": "https://trecobox.com.br/wp-content/uploads/2022/02/Robert-Downey-Jr.-Homem-de-Ferro-capa.jpg"
   },
   {
-    "id":"2",
-    "title": "Nova Série anunciada no Disney +",
-    "description": "bla blabla",
-    "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+    "id": "2",
+    "title": "Loki na Disney",
+    "description": "Disney Anuncia o lançamento de Loki 2, previsto para Março 2023",
+    "photoCover": "https://images.thedirect.com/media/article_full/loki-season-2-filming.jpg",
+    "trailer": "https://www.youtube.com/watch?v=y-szNAHNcCM"
+  },
+  {
+    "id": "3",
+    "title": "Pantera Negra 3",
+    "description": "",
+    "photoCover": "https://musicaecinema.com/wp-content/uploads/2023/02/letiti-fala-sobre-pantera-negra-2-chardwick-boseman-capa.jpeg.webp"
+  },
+  {
+    "id": "4",
+    "title": "Novidades dos XMen",
+    "description": "",
+    "photoCover": "https://legadodamarvel.com.br/wp-content/uploads/2021/01/Novo-filme-dos-X-Men-na-Marvel-Studios-ganha-novidades-legadodamarvel-750x422.jpg"
   }
 ]
+
+
