9186d5fdfdfe501b2ec65f29bf0a2079026bbfb9
Alexandre Carvalho <alex380bb@gmail.com>
2023-10-12 20:17:40 -0300

    update blog

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -2,13 +2,47 @@ export const dataFake = [
   {
     "id":"1",
     "title": "NOVO HOMEM DE FERRO EM 3D",
-    "description": "marvel anuncia um novo filme do homem de ferro, confira",
+    "description": "É possível, sim, que Robert Downey Jr. volte a interpretar o Homem de Ferro no Universo Cinematográfico Marvel (MCU). Depois das notícias sobre supostas negociações entre o astro e a Marvel, o ator Mark Ruffalo falou, em um evento, que a possibilidade do retorno de Robert como Tony Stark realmente existe.Há uma máquina do tempo disponível, além de universos e realidades alternativas, então… Tudo pode acontecer”, sublinhou o ator, durante sua participação na Emerald City Comic-Con. As realidades alternativas são o multiverso, que vem sendo explorado pelo MCU desde a Fase 5 e perdurará até a Fase 6. É a chamada “Saga Multiverso.",
     "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/7F51FA9F6CBD9F0C9B1394B1CC0A6A842D07091318674E234CD33CBF7C28CDC3/scale?width=1200&aspectRatio=1.78&format=jpeg"
   },
   {
     "id":"2",
     "title": "Nova Série anunciada no Disney +",
-    "description": "bla blabla",
+    "description": "Ahsoka: nova série do universo Star Wars chega ao Disney+ em agosto de 2023; veja trailer. A Lucasfilm exibiu no Star Wars Celebration o primeiro trailer da série Ashoka, que será lançada em agosto de 2023 pela Disney Plus",
     "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+  },
+
+  {
+    "id":"3",
+    "title": "Novo filme do pantera negra lançado em breve",
+    "description": "Pantera Negra: Wakanda Para Sempre é a continuação do longa Pantera Negra, da Marvel, dirigido por Ryan Coogler e produzido por Kevin Feige. No filme, o mundo de Wakanda se expande. Após a morte do ator de T'Challa (Chadwick Boseman) o foco de Wakanda Para Sempre são os personagens em volta do Pantera Negra. Rainha Ramonda (Angela Bassett), Shuri (Letitia Wright), M'Baku (Winston Duke), Okoye (Danai Gurira) e as Dora Milage lutam para proteger a nação fragilizada de outros países após a morte de T'Challa. Enquanto o povo de Wakanda se esforça para continuar em frente neste novo capítulo, a família e amigos do falecido rei precisam se unir com a ajuda de Nakia (Lupita Nyong'o), integrante dos Cães de Guerra, e Everett Ross (Martin Freeman). Em meio a isso tudo, Wakanda ainda terá que aprender a conviver com a nação debaixo d'água, Talokan, e seu rei Namor (Tenoch Huerta).",
+    "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/35720ACD323F927C3A83D809D0F460BD3651740DA519BCC184B6F042057EA14D/scale?width=1200&aspectRatio=1.78&format=jpeg"
+  },
+
+  {
+    "id":"4",
+    "title": "Novo filme dos X-men está em pós produção, afirma diretor",
+    "description": "Os lançamentos futuros do MCU já estão agendados até 2027, com filmes como Quarteto Fantástico, A Dinastia Kang e Guerras Secretas. Isso levanta a questão: quando exatamente veremos os X-Men nas telonas? Embora nenhuma data de lançamento tenha sido confirmada, especula-se que o novo filme dos X-Men possa ser lançado em algum momento de 2028, após a conclusão dos projetos já anunciados.",
+    "photoCover":"https://midias.correiobraziliense.com.br/_midias/jpg/2022/07/22/675x450/1_xmen-26096415.jpeg?20220722171339?20220722171339"
+  },
+
+  {
+    "id":"5",
+    "title": "BATMAN PARTE 2 INDICA PRESENCA DE OUTRO VILAO",
+    "description": "A pré-produção de ‘Batman: Parte 2’ está a todo vapor após o fim da Greve de Roteiristas, e os fãs estão ávidos por qualquer detalhe sobre o próximo capítulo da saga do Cavaleiro das Trevas.Rumores recentes sugerem que o antagonista central da continuação dirigida por Matt Reeves pode ser o misterioso vilão conhecido como Silêncio.Jeff Sneider, do podcast The Hot Mic, lançou essa especulação, levantando a possibilidade de que Silêncio assuma um papel significativo na trama",
+    "photoCover":"https://t.ctcdn.com.br/obSTh1skPQyqN8aPdjGJ7wPflSE=/0x183:1380x960/768x432/smart/i566683.jpeg"
+  },
+  {
+    "id":"6",
+    "title": "Elenco de The Boys se choca com HQ violenta: 'Não é para seres humanos",
+    "description": "De acordo com Shetty, a prioridade no momento não é acumular séries que compartilham uma mesma temática, mas encaixar produções com o intuito de formar um grande “quebra-cabeça.Gen V, por exemplo, expande o arco narrativo de Soldier Boy, vivido por Jensen Ackles, além de trazer mais detalhes sobre o composto V e o treinamento de jovens heróis pela Vought.",
+    "photoCover":"https://kanto.legiaodosherois.com.br/w760-h398-gnw-cfill-q95/wp-content/uploads/2023/10/legiao_PygEhVTu9j7z.png.webp"
+  },
+  {
+    "id":"7",
+    "title": "DC Comics anuncia relançamento do selo Elseworlds",
+    "description": "As histórias fora de continuidade sempre foram um pilar da publicação da DC, concedendo aos criadores a capacidade de explorar todos os cantos selvagens do Universo DC”, disse o editor executivo da DC, Ben Abernathy. “Estamos entusiasmados em trazer uma nova gama de títulos sob o selo ELSEWORLDS em 2024, combinando alguns títulos totalmente novos dos principais contadores de histórias, bem como sequências da linha atual da DC que se enquadram na mesma estética fora de continuidade. ",
+    "photoCover":"https://cdn.colombia.com/sdi/2022/09/07/dc-studios-dan-lin-rechazo-jefe-series-peliculas-dc-comics-warner-bros-discovery-1064065.webp"
   }
+
 ]
